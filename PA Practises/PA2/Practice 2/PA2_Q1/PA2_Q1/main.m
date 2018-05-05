9500a2c1988e3b01b26fc8782d0fca1d565395b3
ciyfhx <pehziheng@yahoo.com.sg>
2018-05-04 20:58:25 -0700

    Update

--- PA Practises/PA2/Practice 2/PA2_Q1/PA2_Q1/main.m
+++ PA Practises/PA2/Practice 2/PA2_Q1/PA2_Q1/main.m
@@ -50,20 +50,4 @@ int main(int argc, const char * argv[]) {
         
     }
     return 0;
-}//
-//  main.m
-//  PA2_Q1
-//
-//  Created by Riyfhx on 24/4/18.
-//  Copyright © 2018 Riyfhx. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
-    return 0;
 }
