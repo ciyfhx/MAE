12112aca14dbfd4f1685a356ab535a3923bd2110
ciyfhx <pehziheng@yahoo.com.sg>
2018-05-17 10:00:42 +0800

    Update

---
+++ PA Practises/PA3/Practice 2/Exercise10/Exercise10/device_management.h
@@ -0,0 +1,107 @@
+//
+//  device_management.h
+//  Exercise10
+//
+//  Created by Zi Heng on 17/5/18.
+//  Copyright © 2018 Zi Heng. All rights reserved.
+//
+
+#ifndef device_management_h
+#define device_management_h
+
+#import <Foundation/Foundation.h>
+
+@interface Device : NSObject{
+    NSInteger ID;
+    NSString* brand;
+    NSString* model;
+    CGFloat price;
+}
+
+- (NSInteger) ID;
+- (NSString*) brand;
+- (NSString*) model;
+- (CGFloat) price;
+
+- (void) setID: (NSInteger) newID;
+- (void) setBrand: (NSString*) newBrand;
+- (void) setModel: (NSString*) newModel;
+- (void) setPrice: (CGFloat) newPrice;
+
+- (CGFloat) showDepCost;
+
+@end
+
+@interface Desktop : Device
+
+- (void) showDesktop;
+
+@end
+
+@interface Notebook : Device
+
+- (void) setAllvars;
+
+@end
+
+@implementation Device
+
+- (NSInteger) ID {
+    return ID;
+}
+- (NSString*) brand {
+    return brand;
+}
+- (NSString*) model {
+    return model;
+}
+- (CGFloat) price {
+    return price;
+}
+
+- (void) setID: (NSInteger) newID {
+    ID = newID;
+}
+- (void) setBrand: (NSString*) newBrand {
+    brand = newBrand;
+}
+- (void) setModel: (NSString*) newModel {
+    model = newModel;
+}
+- (void) setPrice: (CGFloat) newPrice {
+    price = newPrice;
+}
+
+- (CGFloat) showDepCost {
+    return price - (price * .2);
+}
+
+@end
+
+@implementation Desktop
+
+- (void) showDesktop {
+    NSLog(@">>> The Desktop Device ID is %li", ID);
+    NSLog(@">>> The Desktop Brand is %@", brand);
+    NSLog(@">>> The Desktop Model is %@", model);
+    NSLog(@">>> The Desktop price is $%.2f", price);
+    NSLog(@">>> The Desktop Device ID is $%.2f", [self showDepCost]);
+}
+
+@end
+
+@implementation Notebook
+
+- (void) setAllvars {
+    ID = arc4random_uniform(991) + 10;
+    brand = @"Apple";
+    model = @"Macbook Pro";
+    price = 3600.0;
+}
+
+@end
+
+
+
+
+#endif /* device_management_h */
---
+++ PA Practises/PA3/Practice 2/Exercise10/Exercise10/main.m
@@ -0,0 +1,38 @@
+//
+//  main.m
+//  Exercise10
+//
+//  Created by Zi Heng on 17/5/18.
+//  Copyright © 2018 Zi Heng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "device_management.h"
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        Desktop* desktop1 = [[Desktop alloc] init];
+        [desktop1 setID: arc4random_uniform(991) + 10];
+        [desktop1 setBrand: @"Dell"];
+        [desktop1 setModel: @"Optimum 2000"];
+        [desktop1 setPrice: 1800.0];
+        
+        [desktop1 showDesktop];
+        
+        NSLog(@">>>--------------------------------------------------------");
+        
+        Notebook* notebook1 = [[Notebook alloc] init];
+        [notebook1 setAllvars];
+        
+        NSLog(@">>> The Notebook Device ID is %li", [notebook1 ID]);
+        NSLog(@">>> The Notebook Brand is %@", [notebook1 brand]);
+        NSLog(@">>> The Notebook Model is %@", [notebook1 model]);
+        NSLog(@">>> The Notebook price is $%.2f", [notebook1 price]);
+        NSLog(@">>> The Notebook Device ID is $%.2f", [notebook1 showDepCost]);
+        
+        
+    }
+    return 0;
+}
